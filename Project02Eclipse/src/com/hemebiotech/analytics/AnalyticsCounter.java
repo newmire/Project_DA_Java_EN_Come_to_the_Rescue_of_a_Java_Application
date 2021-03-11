69cea2a66080fdc90324a5098cdc9149f868b4f5
Rémi Bouchard <remibouchard@hotmail.fr>
2021-03-11 20:01:06 +0100

    spliting main method

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,44 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+    private TreeMap<String, Integer> map;
+    private final ISymptomReader reader;
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+    public AnalyticsCounter(ISymptomReader reader) {
+        this.reader = reader;
+        this.map = new TreeMap<>();
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+    }
+
+    public List<String> read() {
+        return reader.getSymptoms();
+    }
+
+    public void compute(List<String> symptoms) {
+        for (String symptom : symptoms) {
+            Integer occurence = this.map.get(symptom);
+            if (occurence == null) {
+                this.map.put(symptom, 1);
+            } else {
+                occurence = occurence + 1;
+                this.map.put(symptom, occurence);
+            }
+        }
+    }
+
+    public void write() throws IOException {
+        String text = this.map.entrySet().stream().map(e -> e.getKey()+" : "+e.getValue()).reduce((a,b)->a+"\n"+b).orElse("");
+        BufferedWriter writer = new BufferedWriter(new FileWriter("result.out"));
+        writer.write(text);
+        writer.close();
+
+    }
 }
